3ea4d3ea92ef7ea6c48945a5e5eb9e8dcfdebdd0
Peng He <105131054+hep905@users.noreply.github.com>
2022-07-12 21:56:19 -0400

    Peng ML case 1
    First trial on ML case 1

---
+++ Example in Josh's slide.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(tidymodels)
+
+d <- read_csv("lab-1/data/ngsschat-processed-data.csv")
+
+d
+
+train_test_split <- initial_split(d, prop = .70)
+data_train <- training(train_test_split)
+data_test <- testing(train_test_split)
+
+my_rec <- recipe(code ~ ., data = data_train)
+
+train_test_split
+
+data_train
+
+data_test
+
+my_rec
+
+# specify model
+my_mod <-
+    logistic_reg() %>% 
+    set_engine("glm") %>%
+    set_mode("classification")
+
+my_mod
+
+# specify workflow
+my_wf <-
+    workflow() %>%
+    add_model(my_mod) %>% 
+    add_recipe(my_rec)
+
+my_wf
+
+fitted_model <- fit(my_wf, data = data_train)
+
+final_fit <- last_fit(fitted_model, train_test_split)
+
+final_fit 
+
+final_fit %>%
+    collect_metrics()
